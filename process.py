2903e58b7a98f6acf7cc0fe9956292915ee03800
Linh Phan <linhphanhust@gmail.com>
2018-04-17 14:52:28 +0700

    process complete, reinit folder

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 import re
 import nltk
 import emoji
+import yaml
 
 
 def remove_special_text(tweet):
@@ -11,12 +12,11 @@ def remove_special_text(tweet):
 def load_emoji(filename):
     
     dict_emoji = {}
-    with open(filename) as emoji_sys_file:
-        for line in emoji_sys_file:
+    with open(filename) as emoji_file:
+        for line in emoji_file:
             line = line[:-1]
             emoji_sysbol, text = line.partition("\t")[::2]
             dict_emoji[emoji_sysbol.strip()] = text
-    # print(dict_emoji)
     return dict_emoji
 
 def replace_emoji(tokens, dict_emoji):
@@ -24,53 +24,83 @@ def replace_emoji(tokens, dict_emoji):
     for emoji in dict_emoji:
         tokens = tokens.replace(emoji, dict_emoji[emoji])
 
-    # tokens = [token if not token in dict_emoji else dict_emoji[token] for token in tokens]
     return tokens
 
 def load_dictionary(filename):
     dic = [line.strip() for line in open(filename, 'r')]
     return dic
 
+def load_boost_word(filename):
+    boost_word = {}
+    with open(filename) as boost_file :
+        for line in boost_file:
+            line = line[:-1]
+            word, score = line.partition("\t")[::2]
+            boost_word[word.strip()] = int(score)
+    return boost_word
 
-def evalue_score(tokens, positve_dict, negative_dict):
-    
+
+def boost_word_score(i, tokens):
+    word = ""
+    if i>0:
+        word = tokens[i-1]
+
+    if word in boost_word:
+        return boost_word[word]
+    else:
+        return 0
+
+def evalue_score(tokens):
     pos_score = 0
     neg_score = 0
-    for word in tokens:
+    for i,word in enumerate(tokens):
         if word in positve_dict:
-            pos_score = pos_score +1
-            print(word)
+            pos_score += (1 + boost_word_score(i, tokens) )
         elif word in negative_dict:
-            neg_score = neg_score +1
-    print("pos_score: ", pos_score, "neg: ", neg_score)
-    return pos_score - neg_score
+            neg_score += (1 + boost_word_score(i, tokens))
+    
+    # print("Pos: ", pos_score, "Neg: ", neg_score)
+    return pos_score , neg_score
+
+def pre_process(tweet):
+    
+    tweet_demoji = emoji.demojize(tweet)            # Convert emojition to text
+    dict_emoji = load_emoji('dictionary/emoji.txt')            # Load file emoji
+    tokens =replace_emoji(tweet_demoji, dict_emoji) # Replace emoji to text
+    
+    tokens = remove_special_text(tokens)            # Remove special text
+    tokens = nltk.word_tokenize(tokens)             # Tokenize tweet
+    
+    return tokens
+
+def process_dataset(input_file):
+    
+    with open(input_file, "r") as dataset :
+        data = yaml.load(dataset)
+        print(len(data))
+        id =0
+        str_file = "ID\tPositive_score\tNegative_score\tContent\n"
+
+        for tweet in data:
+            tokens = pre_process(tweet)
+            #Evalue score:
+            pos_score, neg_score = evalue_score(tokens)
+            
+            str_file += str(id) + "\t"+ str(pos_score) +"\t" + str(neg_score) + "\t" + tweet + "\n"
+            id+=1
+
+    text_file = open("output.txt", "w")
+    text_file.write(str_file)
+    text_file.close()
 
 
+# tokens = pre_process(tweet)
+# print(tokens)
 
-text = 'RT @tom_watson: face_with_tears_of_joy aight Russia \u201Cwe\u2019ll give you evidence \U0001F609\U0001F609\u201D on UK air strikes in Syria very good. https://t.co/Pf45W4I7wZ dsds'
+#Load file:
+positve_dict = load_dictionary('dictionary/positive-words.txt')
+negative_dict = load_dictionary('dictionary/negative-words.txt')
+boost_word = load_boost_word('dictionary/boost-word.txt')
 
 
-
-
-text_demoji = emoji.demojize(text)
-
-dict_emoji = load_emoji('emoji.txt')
-tokens = replace_emoji(text_demoji, dict_emoji)
-tokens = remove_special_text(tokens)
-
-tokens = nltk.word_tokenize(tokens)
-
-positve_dict = load_dictionary('positive-words.txt')
-negative_dict = load_dictionary('negative-words.txt')
-
-# positve_dict = load_dictionary('test.txt')
-# negative_dict = load_dictionary('test.txt')
-
-score = evalue_score(tokens, positve_dict, negative_dict)
-
-if score > 0 :
-    print("Positive!")
-elif score <0 :
-    print("Negative!")
-else:
-    print("Neutral!")
+process_dataset('syria2.yaml')
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
-# import yaml
+import yaml
 # import emoji
 import re
 
-# with open("syria2.yaml", "r") as input_file:
-#     data = yaml.load(input_file)
-#     # print(len(data))
-#     # for item in data:
-#     #     print (item)
-#     # print(data[1])
+with open("syria2.yaml", "r") as input_file:
+    data = yaml.load(input_file)
+    print(len(data))
+    for item in data:
+        print (item)
+    print(data[1])
+
 
 # print(emoji.demojize( '\u2019 :face_with_tears_of_joy:'))
 
@@ -24,14 +25,26 @@ import re
 #     print(myvars['121'])
 
 
-def remove_special_text(tweet):
-    # tokens = tweet.split(' ')
-    tokens = re.sub(r'http\S+', '', tweet)
-    URLless_string = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet)
-    print(URLless_string)
-    print(tokens)
-    return tokens
+# def remove_special_text(tweet):
+#     # tokens = tweet.split(' ')
+#     tokens = re.sub(r'http\S+', '', tweet)
+#     URLless_string = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet)
+#     print(URLless_string)
+#     print(tokens)
+#     return tokens
 
-text = 'RT @tom_watson: face_with_tears_of_joy afaik Link to the full legal advice on UK air strikes in Syria. https://t.co/Pf45W4I7wZ sdsds'
+# text = 'RT @tom_watson: face_with_tears_of_joy afaik Link to the full legal advice on UK air strikes in Syria. https://t.co/Pf45W4I7wZ sdsds'
 
-print(remove_special_text(text))
+# print(remove_special_text(text))
+
+# def load_boost_word(filename):
+#     boost_word = {}
+#     with open(filename) as boost_file :
+#         for line in boost_file:
+#             line = line[:-1]
+#             word, score = line.partition("\t")[::2]
+#             boost_word[word.strip()] = int(score)
+#     return boost_word
+
+# boost = load_boost_word('boost-word.txt')
+# print(boost)
